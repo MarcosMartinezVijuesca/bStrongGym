a9757933c6f9e635770027d895b12a40bea3482f
MarcosMartinezVijuesca <martinezvijuescamarcos@gmail.com>
2025-12-27 17:02:12 +0100

    Initial project structure with docker-compose and mariaDB configured

---
+++ src/main/java/com/gym/bstrong/BstrongApplication.java
@@ -0,0 +1,13 @@
+package com.gym.bstrong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstrongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstrongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/bstrong/BstrongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.bstrong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BstrongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
